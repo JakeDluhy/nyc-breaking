45e253f0df9d9cb91b04f2bc6290dc0d398e209f
Jake Dluhy <dluhy.jake@gmail.com>
2017-08-10 10:31:18 -0700

    Add readme, and babel-core for using register

---
+++ README.md
@@ -0,0 +1,3 @@
+# NYC Breaking
+
+This repo is to demonstrate a minimal failing test case for `nyc` version `11.x.x`. To see the error, run `npm install` followed by `npm test`
--- test/run-tests.js
+++ test/run-tests.js
@@ -1,1 +1,5 @@
+// Set up babel compiling
+require('babel-core/register');
+
+// Require in the ES6 module library. babel-core/register _should_ handle the transpiling
 require('../mirage').default;
